fcd0094451b5d4bd8180cb357dc87e238549c633
Yan White <yanwhite@Yans-MacBook-Pro-2.local>
2025-04-24 20:26:00 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,31 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { imagetools } from 'vite-imagetools';
 
-// https://astro.build/config
-export default defineConfig({});
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  vite: {
+    build: {
+      // This is crucial for controlling output
+      rollupOptions: {
+        output: {
+          entryFileNames: 'main.js',       // Keep original name
+          chunkFileNames: '[name].[hash].js', // For chunks
+          assetFileNames: '[name][extname]'  // For assets
+        }
+      },
+      outDir: 'dist/assets',              // Where Vite puts assets
+      assetsDir: ''                      // No extra directory
+    },
+    resolve: {
+      alias: {
+        '@bootstrap': '/node_modules/bootstrap/scss',
+        '@fonts': '/src/assets/fonts',
+      },
+    },
+    plugins: [imagetools()]
+  },
+
+  integrations: [sitemap()]
+});
---
+++ src/assets/images/images.md
@@ -0,0 +1,7 @@
+---
+title: "Wish"
+thumb: "/images/art1-thumb.jpg"
+full: "/images/wish.jpg"
+tags: ["painting", "2025", "Acrylic paint and marble dust on canvas", "90 x 110cm"]
+---
+
